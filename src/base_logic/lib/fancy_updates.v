27dac1e4f855bbc350c99b9771c37cc4394fff67
Tej Chajed <chajed@wisc.edu>
2025-07-15 09:56:36 -0500

    Fix use of coPset internals

--- src/base_logic/lib/fancy_updates.v
+++ src/base_logic/lib/fancy_updates.v
@@ -7,32 +7,28 @@ From iris.prelude Require Import options.
 Export invGS.
 Import uPred le_upd.
 
-Definition coPNode' := coPset.coPNode'.
-Definition coPset_wf := coPset.coPset_wf.
+Arguments coPset.coPset_wf !_ / : simpl nomatch, assert.
+Arguments coPset.coPNode' : simpl never.
+Hint Resolve coPset.coPNode'_wf : core.
 
 (** * Suffix subsets *)
 Fixpoint coPset_suffixes_of_raw (p : positive) (E: coPset_raw) : coPset_raw :=
   match p with
   | 1 => E
-  | p~0 => coPNode' false (coPset_suffixes_of_raw p E) (coPLeaf false)
-  | p~1 => coPNode' false (coPLeaf false) (coPset_suffixes_of_raw p E)
+  | p~0 => coPset.coPNode' false (coPset_suffixes_of_raw p E) (coPLeaf false)
+  | p~1 => coPset.coPNode' false (coPLeaf false) (coPset_suffixes_of_raw p E)
   end%positive.
-Lemma coPset_suffixes_of_wf p E : coPset_wf E → coPset_wf (coPset_suffixes_of_raw p E).
-Proof.
-  induction p; simpl; try eauto; intros H.
-  - destruct (coPset_suffixes_of_raw p E); [destruct b|]; eauto.
-  - destruct (coPset_suffixes_of_raw p E); [destruct b|]; simpl; eauto.
-Qed.
+Lemma coPset_suffixes_of_wf p E : coPset.coPset_wf E → coPset.coPset_wf (coPset_suffixes_of_raw p E).
+Proof. induction p; simpl; eauto. Qed.
 Definition coPset_suffixes_of (p : positive) (E : coPset) : coPset :=
   coPset_suffixes_of_raw p (`E) ↾ coPset_suffixes_of_wf _ _ (proj2_sig E).
 Lemma elem_coPset_suffixes_of p q E : p ∈ coPset_suffixes_of q E ↔ ∃ q', (p = Pos.app q' q) ∧ q' ∈ E.
 (* Proof.
   unfold elem_of, coPset_elem_of; simpl; split.
   - revert p; induction q; intros [?|?|]; simpl;
-      rewrite ?coPset.coPset_elem_of_node /=. ; naive_solver.
-  - by intros [q' (->&Helem)]; induction q; simpl; rewrite ?coPset_elem_of_node.
-Qed. *)
-Admitted.
+      rewrite ?coPset.coPset_elem_of_node; naive_solver.
+  - by intros [q' (->&Helem)]; induction q; simpl; rewrite ?coPset.coPset_elem_of_node.
+Qed.
 Lemma coPset_suffixes_of_top p :
   coPset_suffixes_of p ⊤ = coPset_suffixes p.
 Proof.
@@ -46,10 +42,9 @@ Definition coPset_inr (E: coPset) : coPset := coPset_suffixes_of (positives_flat
 Lemma coPset_suffixes_of_infinite p E:
   (¬ set_finite E) → (¬ set_finite (coPset_suffixes_of p E)).
 Proof.
-  (* rewrite ?coPset_finite_spec; simpl. intros Hsuff.
-  induction p; simpl; rewrite ?coPset_finite_node; rewrite ?andb_True //=; naive_solver.
-Qed. *)
-Admitted.
+  rewrite ?coPset.coPset_finite_spec; simpl. intros Hsuff.
+  induction p; simpl; rewrite ?coPset.coPset_finite_node; rewrite ?andb_True //=; naive_solver.
+Qed.
 
 Lemma coPset_inl_inr_disj E1 E2 :
   coPset_inl E1 ## coPset_inr E2.
